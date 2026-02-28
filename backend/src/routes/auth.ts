2583d904ed19a59c60fabf59147357a53187dc0f
Mike Ryb <myhou@mail.ru>
2026-02-28 12:28:34 +0300

    CSRFadd

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -9,11 +9,17 @@ import {
     updateCurrentUser,
 } from '../controllers/auth'
 import auth from '../middlewares/auth'
+import csrf from 'csurf'
+
+const csrfProtection = csrf({ cookie: true })
 
 const authRouter = Router()
 
 authRouter.get('/user', auth, getCurrentUser)
-authRouter.patch('/me', auth, updateCurrentUser)
+authRouter.get('/csrf-token', csrfProtection, (req, res) => {
+    res.send(req.csrfToken())
+})
+authRouter.patch('/me', auth, csrfProtection, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
 authRouter.post('/login', login)
 authRouter.get('/token', refreshAccessToken)
