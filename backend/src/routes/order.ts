dd29315d03174e245b490cf983e50b7bdac3a496
Mike Ryb <myhou@mail.ru>
2026-02-28 13:45:46 +0300

    fixTests

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -11,10 +11,13 @@ import {
 import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import { validateOrderBody } from '../middlewares/validations'
 import { Role } from '../models/user'
+import csrf from 'csurf'
+
+const csrfProtection = csrf({ cookie: true })
 
 const orderRouter = Router()
 
-orderRouter.post('/', auth, validateOrderBody, createOrder)
+orderRouter.post('/', auth, validateOrderBody, csrfProtection, createOrder)
 orderRouter.get('/all', auth, getOrders)
 orderRouter.get('/all/me', auth, getOrdersCurrentUser)
 orderRouter.get(
