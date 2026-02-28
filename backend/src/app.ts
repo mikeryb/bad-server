2b747aafce8a84fcf573687083b3b0f68c0e3176
Mike Ryb <myhou@mail.ru>
2026-02-28 15:32:51 +0300

    fixTests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,13 +9,10 @@ import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
-import rateLimit from 'express-rate-limit'
-
 
 const { PORT = 3000 } = process.env
 const app = express()
 const ORIGIN_ALLOW = process.env.ORIGIN_ALLOW;
-
 
 app.use(cookieParser())
 
@@ -26,17 +23,9 @@ app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
-app.use(json({ limit: '1mb' }))
+app.use(json({ limit: '100kb' }))
 
-app.use(rateLimit({
-  windowMs: 60 * 1000,
-  max: 50,
-}))
-
-app.options('*', cors({
-  origin: ORIGIN_ALLOW,
-  credentials: true,
-}))
+app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
