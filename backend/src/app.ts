783e23bb4ba160de03862c19a8d5f600923d681d
Mike Ryb <myhou@mail.ru>
2026-03-01 11:53:55 +0300

    fixTests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,7 +25,7 @@ app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
-app.use(json({ limit: '10kb' }))
+app.use(json({ limit: '50kb' }))
 
 app.use(rateLimit({
   windowMs: 60 * 1000,
